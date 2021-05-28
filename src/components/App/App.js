c3855b3c0b7adfe7452d10439294e3e83e4f525d
Pawel Switalski <pawelswitalski94@gmail.com>
2021-05-28 15:04:33 +0200

    install font-awesome

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
+import Navigation from 'components/Navigation/Navigation';
+import Header from 'components/Header/Header';
 import Home from 'routes/Home/Home';
 
 import 'components/App/App.module.scss';
@@ -7,7 +15,15 @@ import 'components/App/App.module.scss';
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Router>
+        {/* <Navigation /> */}
+        <Header />
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { header } from 'components/Header/Header.module.scss';
+
+const Header = () => {
+    return(
+        <header className={header}>Beer Recipe App</header>
+    )
+}
+
+export default Header;
