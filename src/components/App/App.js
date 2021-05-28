eb66ba55652325fd059f1401265598a084761fab
Pawel Switalski <pawelswitalski94@gmail.com>
2021-05-28 16:04:00 +0200

    handle open menu

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -16,7 +16,7 @@ function App() {
   return (
     <div className="App">
       <Router>
-        {/* <Navigation /> */}
+        <Navigation />
         <Header />
         <Switch>
           <Route exact path='/'>
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronCircleRight } from '@fortawesome/free-solid-svg-icons'
+
+import Button from 'components/Button/Button';
+
+import { nav, navActive, openMenu, openMenuActive } from 'components/Navigation/Navigation.module.scss';
 
 const Navigation = () => {
+
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const openMenuHandler = () => {
+        setMenuOpen((prevState) => !prevState);
+    }
+
     return (
-        <nav>Nawigacja</nav>
+        <nav className={menuOpen ? `${nav} ${navActive}` : `${nav}`} >
+            <FontAwesomeIcon className={menuOpen ? `${openMenu} ${openMenuActive}` : `${openMenu}`} onClick={openMenuHandler} icon={faChevronCircleRight} size={'2x'} />
+           <Button content="Log in" />
+           <Button content="Create account" />
+        </nav>
     )
 }
 
