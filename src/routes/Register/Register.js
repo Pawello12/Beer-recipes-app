045993ccbf289611dc7ec6f2b072f3e81f77fa2c
Pawel Switalski <pawelswitalski94@gmail.com>
2021-05-31 16:36:48 +0200

    create register view

--- src/routes/Register/Register.js
+++ src/routes/Register/Register.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
+import Button from 'components/Button/Button';
+
+import {register} from 'routes/Register/Register.module.scss';
+
 const Register = () => {
     return(
-        <div>Rejestracja</div>
+    <div className={register}>
+        <h2>Create account</h2>
+        <label htmlFor="login">Username:</label>
+        <input type="text" id="login" />
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" />
+        <label htmlFor="repeat-password">Repeat password:</label>
+        <input type="password" id="repeat-password" />
+        <Button content="Log In" />
+    </div>
     )
 }
 
