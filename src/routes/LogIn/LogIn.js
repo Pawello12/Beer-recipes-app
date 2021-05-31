f0457b0678a7757933bb52ef767b29d70ba928a7
Pawel Switalski <pawelswitalski94@gmail.com>
2021-05-31 16:30:38 +0200

    add background image, create login screen

--- src/routes/LogIn/LogIn.js
+++ src/routes/LogIn/LogIn.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
+import Button from 'components/Button/Button';
+
+import {logIn} from 'routes/LogIn/LogIn.module.scss';
+
 const LogIn = () => {
     return(
-        <div>logowanie</div>
+        <div className={logIn}>
+            <h2>Log In</h2>
+            <label htmlFor="login">Login:</label>
+            <input type="text" id="login" />
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" />
+            <Button content="Log In" />
+        </div>
     )
 }
 
