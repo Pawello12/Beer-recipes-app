00c0db56808ad5665ddc6404dcdafb007b2dbd38
Pawel Switalski <pawelswitalski94@gmail.com>
2021-06-01 14:35:03 +0200

    create login handler

--- src/routes/LogIn/LogIn.js
+++ src/routes/LogIn/LogIn.js
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+import { Redirect } from 'react-router-dom';
 
 import Button from 'components/Button/Button';
+import ErrorMessage from 'components/ErrorMessage/ErrorMessage';
 
 import {logIn} from 'routes/LogIn/LogIn.module.scss';
+
+const loginUrl = 'http://localhost:1337/auth/local';
 
 const initialInputValue = {
     login: '',
@@ -12,6 +17,8 @@ const initialInputValue = {
 const LogIn = () => {
 
     const [inputValue, setInputValue] = useState(initialInputValue);
+    const [errorMessage, setErrorMessage] = useState('');
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
     const updateInputValue = (e) => {
         setInputValue({
@@ -21,7 +28,35 @@ const LogIn = () => {
     }
 
     const verifyLoginForm =() => {
+        if (inputValue.login === '' || inputValue.password === '') {
+            setErrorMessage('You must provide all the information.')
+            return false;
+        } else {
+            setErrorMessage('')
+            return true;
+        }
+    }
 
+    const logInHandler = (e) => {
+        e.preventDefault();
+        const isApproved = verifyLoginForm();
+        if (isApproved) {
+            axios.post(loginUrl, {
+                identifier: inputValue.login,
+                password: inputValue.password
+            })
+            .then(response => {
+                console.log(response);
+                setErrorMessage('');
+                localStorage.setItem('token', response.data.jwt);
+                localStorage.setItem('username', response.data.user.username);
+                setIsLoggedIn(true);
+            })
+            .catch(error => {
+                console.log(error.response);
+                setErrorMessage(error.response.data.data[0].messages[0].message);
+            })
+        }
     }
 
     return(
@@ -31,7 +66,9 @@ const LogIn = () => {
             <input type="text" id="login" value={inputValue.login} onChange={updateInputValue} />
             <label htmlFor="password">Password:</label>
             <input type="password" id="password" value={inputValue.password} onChange={updateInputValue} />
-            <Button content="Log In" type="submit" />
+            <ErrorMessage error={errorMessage} />
+            <Button content="Log In" clickHandler={logInHandler} type="submit" />
+            {isLoggedIn ? <Redirect to="/search" /> : null}
         </form>
     )
 }
